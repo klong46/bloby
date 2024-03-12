3eed72c0b96a9c882baf5ff6e27f21bbf5da9b33
klong46 <kyledlong1@gmail.com>
2024-03-12 15:52:22 -0400

    add scrolling to level select

--- Source/levelSelect.lua
+++ Source/levelSelect.lua
@@ -3,13 +3,18 @@ import "constants"
 import "levelSelectTile"
 
 local NUM_COLS = 5
-local NUM_ROWS = 3
+local NUM_ROWS = math.floor(TOTAL_LEVELS/NUM_COLS)
+local ROWS_ON_SCREEN = 3
+
 
 local function getLevelNum(x, y)
     return math.floor(((y-1)*NUM_COLS)+x)
 end
 
 local currentLevel = 1
+local scrollDown = true
+local offset = 0
+local scrollQueued = false
 
 class('LevelSelect').extends(SLIB)
 
@@ -20,7 +25,9 @@ function LevelSelect:init(startingLevel, scores)
     self.cursorPos = PD.geometry.point.new(1,1)
     self.previousSelected = PD.geometry.point.new(1,1)
     self.tiles = {}
+    self.scrollAnimator = nil
     self:addLevelTiles()
+    self.tiles[getLevelNum(self.cursorPos.x, self.cursorPos.y)]:select()
     self:add()
 end
 
@@ -43,7 +50,13 @@ end
 function LevelSelect:cursorLeft()
     if getLevelNum(self.cursorPos.x, self.cursorPos.y) > 1 then
         self:setPreviousSelected()
-        self.cursorPos.x -= 1
+        if self.cursorPos.x % NUM_COLS == 1 then
+            self.cursorPos.y -= 1
+            self.cursorPos.x = NUM_COLS
+            self:checkScrollUp()
+        else
+            self.cursorPos.x -= 1
+        end
         self:updateSelectTiles()
     end
 end
@@ -52,7 +65,13 @@ function LevelSelect:cursorRight()
     if getLevelNum(self.cursorPos.x, self.cursorPos.y) < TOTAL_LEVELS and
        getLevelNum(self.cursorPos.x, self.cursorPos.y) < currentLevel then
         self:setPreviousSelected()
-        self.cursorPos.x += 1
+        if self.cursorPos.x % NUM_COLS == 0 then
+            self.cursorPos.y += 1
+            self.cursorPos.x = 1
+            self:checkScrollDown()
+        else
+            self.cursorPos.x += 1
+        end
         self:updateSelectTiles()
     end
 end
@@ -63,6 +82,7 @@ function LevelSelect:cursorDown()
         self:setPreviousSelected()
         self.cursorPos.y += 1
         self:updateSelectTiles()
+        self:checkScrollDown()
     end
 end
 
@@ -71,6 +91,19 @@ function LevelSelect:cursorUp()
         self:setPreviousSelected()
         self.cursorPos.y -= 1
         self:updateSelectTiles()
+        self:checkScrollUp()
+    end
+end
+
+function LevelSelect:checkScrollDown()
+    if offset < NUM_ROWS-ROWS_ON_SCREEN then
+        self:scrollDown()
+    end
+end
+
+function LevelSelect:checkScrollUp()
+    if offset > 0 then
+        self:scrollUp()
     end
 end
 
@@ -87,4 +120,53 @@ end
 function LevelSelect:select()
     StartGame(getLevelNum(self.cursorPos.x, self.cursorPos.y))
     self:remove()
+end
+
+function LevelSelect:scrollDown()
+    scrollDown = true
+    offset += 1
+    self:startAnimator()
+end
+
+function LevelSelect:scrollUp()
+    scrollDown = false
+    offset -= 1
+    self:startAnimator()
+end
+
+function LevelSelect:startAnimator()
+    if not self.scrollAnimator then
+        self.scrollAnimator = GFX.animator.new(500, 0, 80, playdate.easingFunctions.outCubic)
+    else
+        if self.scrollAnimator:ended() then
+            self.scrollAnimator:reset()
+        else
+            print('asdf')
+            scrollQueued = true
+        end
+    end
+end
+
+function LevelSelect:update()
+    LevelSelect.super.update(self)
+    if scrollQueued then
+        self.scrollAnimator:reset()
+        scrollQueued = false
+    end
+    if self.scrollAnimator and (not self.scrollAnimator:ended()) then
+        for y = 1, NUM_ROWS, 1 do
+            for x = 1, NUM_COLS, 1 do
+                local number = self.tiles[getLevelNum(x,y)].numberLabel
+                local yPos
+                if scrollDown then
+                    yPos = ((y-offset+1)*80-55) - self.scrollAnimator:currentValue()
+                else
+                    yPos = ((y-offset-1)*80-55) + self.scrollAnimator:currentValue()
+                end
+                number:moveTo(number.x, yPos)
+                local tile = self.tiles[getLevelNum(x,y)]
+                tile:moveTo(tile.x, yPos+15)
+            end
+        end
+    end
 end
