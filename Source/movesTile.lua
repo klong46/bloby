1117137c33a70b83f0999f4d98d1b910f686a64e
klong46 <kyledlong1@gmail.com>
2024-04-21 21:20:17 -0400

    add moves positions to levels

--- Source/movesTile.lua
+++ Source/movesTile.lua
@@ -8,8 +8,8 @@ class('MovesTile').extends(SLIB)
 
 function MovesTile:init(position)
     MovesTile.super.init(self)
-    self:setSize(25, 20)
-    self:moveTo((position.x * TILE_SIZE) - 3, (position.y * TILE_SIZE) - 7)
+    self:setSize(30, 20)
+    self:moveTo((position.x * TILE_SIZE) - 1, (position.y * TILE_SIZE) - 7)
     self:setZIndex(2)
     self:add()
 end
@@ -22,5 +22,10 @@ end
 function MovesTile:draw()
     GFX.setImageDrawMode(GFX.kDrawModeInverted)
     GFX.setFont(font)
-    GFX.drawText(""..(Turn-1).."", 0, 0)
+    local move = Turn-1
+    if move > 999 then
+        GFX.drawText("999+", 0, 0)
+    else
+        GFX.drawText(""..move.."", 0, 0)
+    end
 end
